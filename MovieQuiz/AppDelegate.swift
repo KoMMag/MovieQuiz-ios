ee77d1d0ebc06c90d0a01fd58eaeeb4340c3df39
KoMMag <kostorv.muhamed@yandex.ru>
2023-11-10 13:57:47 +0300

    Итоговый вариант

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -24,4 +24,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didDiscardSceneSessions sceneSessions: Set<UISceneSession>
     ) {}
+    
+
+
 }
